47cd1aefa5c245e896e3e10376cea3169738c1e1
aarush-narang <70560514+aarush-narang@users.noreply.github.com>
2022-06-26 22:30:31 +0000

    Deploying to gh-pages from @ aarush-narang/Portfolio@c7ee66d90c5bd27cf4276889d3bb0cd2431063c6 🚀

---
+++ _next/static/SLgp9HE0rUH_HgLYkhVKf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d54714afb5ee2334.css","static/chunks/pages/index-40187fa51c04102a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
