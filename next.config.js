c65f357b1b7b3eaf3dbfed43b477e413c23e4ae2
aarush-narang <aarushnarang@gmail.com>
2023-01-22 12:27:24 -0800

    move portfolio to root path

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  redirects: async () => {
-    return [
-      {
-        source: "/",
-        destination: `${process.env.NEXT_PUBLIC_BASE_PATH}`,
-        permanent: true,
-      },
-    ];
-  },
+  // redirects: async () => {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: `${process.env.NEXT_PUBLIC_BASE_PATH}`,
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
